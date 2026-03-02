3015ae3a8c25f29516cb3ad822b3a956c7e7c42a
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-03-02 22:49:49 +0530

    to convert fahrenheit temp to celsius temp

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,21 +4,13 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        double number1 = sc.nextDouble();
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = sc.nextDouble();
 
-        System.out.print("Enter second number: ");
-        double number2 = sc.nextDouble();
+        double celsiusResult = (fahrenheit - 32) * 5 / 9;
 
-        double addition = number1 + number2;
-        double subtraction = number1 - number2;
-        double multiplication = number1 * number2;
-        double division = number1 / number2;
-
-        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
-                + number1 + " and " + number2 + " is "
-                + addition + ", " + subtraction + ", "
-                + multiplication + ", and " + division);
+        System.out.println("The " + fahrenheit + " fahrenheit is "
+                + celsiusResult + " celsius");
 
         sc.close();
     }
