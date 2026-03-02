ca73b04c34099693174cbf072100af434df9605e
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-03-02 22:46:57 +0530

    to calculate add,sub,mul,div using calculator

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,20 +4,21 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter base in cm: ");
-        double base = sc.nextDouble();
+        System.out.print("Enter first number: ");
+        double number1 = sc.nextDouble();
 
-        System.out.print("Enter height in cm: ");
-        double height = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double number2 = sc.nextDouble();
 
-        double areaCm = 0.5 * base * height;
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
 
-        // 1 inch = 2.54 cm
-        // So 1 sq inch = (2.54 * 2.54) sq cm
-        double areaInches = areaCm / (2.54 * 2.54);
-
-        System.out.println("The Area of the triangle in sq in is "
-                + areaInches + " and sq cm is " + areaCm);
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + addition + ", " + subtraction + ", "
+                + multiplication + ", and " + division);
 
         sc.close();
     }
