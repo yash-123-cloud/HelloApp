042b67ce6070baa9310534dc526efbc6dfb438ec
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-03-16 18:14:08 +0530

    Added HelloApp project

--- HelloApp/HelloApp.java
+++ HelloApp/HelloApp.java
@@ -1,7 +1,11 @@
+ import java.util.Scanner;
+
 public class HelloApp {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+    }
 }
