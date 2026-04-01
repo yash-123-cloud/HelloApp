b71557e1d6c410f643fde42ce2434e62805afc0c
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-04-01 15:38:44 +0530

    UC7:to print hello world!

--- HelloApp/HelloApp.java
+++ HelloApp/HelloApp.java
@@ -1,24 +1,20 @@
-  public class HelloApp {
+public class HelloApp {
 
     public static void main(String[] args) {
 
-        // If no arguments are provided
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+        String message;
+
+        // Check if arguments are provided
+        if (args.length > 0) {
+            // Join names with comma
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
+        } else {
+            // Default message
+            message = "Hello, World!";
         }
 
-        // Build names using StringBuilder
-        StringBuilder nameBuilder = new StringBuilder();
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // Remove trailing ", "
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-        // Print final greeting
-        System.out.println("Hello, " + names + "!");
+        // Print result
+        System.out.println(message);
     }
 }
