f579d9f7894a3a422e0fdd9b8e8ba91768d803ea
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-04-01 21:32:41 +0530

    branch3:to calculate the students who can vote

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,13 +4,24 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter temperature in Celsius: ");
-        double celsius = sc.nextDouble();
+        int[] ages = new int[10];
 
-        double fahrenheitResult = (celsius * 9 / 5) + 32;
+        // Taking input
+        System.out.println("Enter ages of 10 students:");
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = sc.nextInt();
+        }
 
-        System.out.println("The " + celsius + " celsius is "
-                + fahrenheitResult + " fahrenheit");
+        // Checking eligibility
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Invalid age: " + ages[i]);
+            } else if (ages[i] >= 18) {
+                System.out.println("The student with age " + ages[i] + " can vote.");
+            } else {
+                System.out.println("The student with age " + ages[i] + " cannot vote.");
+            }
+        }
 
         sc.close();
     }
